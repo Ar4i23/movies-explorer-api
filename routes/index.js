6d2c3efe99fa22fdda906270cd0309ee7390963a
Arthur Antonov <artur-antonov-1992@mail.ru>
2023-10-10 20:39:24 +0300

    добавил переместил валидацию в папку validation

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
-const userRouter = require('./users');
-const movieRouter = require('./movies');
-const signupRouter = require('./signup');
-const signinRouter = require('./signin');
+const usersRouter = require('../validation/users');
+const moviesRouter = require('../validation/movies');
+const signupRouter = require('../validation/signup');
+const signinRouter = require('../validation/signup');
 const auth = require('../middlewares/auth');
 const NotFountError = require('../errors/NotFountError');
 
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
 router.use(auth);
-router.use('/users', userRouter);
-router.use('/movies', movieRouter);
+router.use('/users', usersRouter);
+router.use('/movies', moviesRouter);
 router.use('*', (req, res, next) => {
   next(new NotFountError('Страница не найдена'));
 });
--- validation/movies.js
+++ validation/movies.js
@@ -29,7 +29,7 @@ router.post(
       nameEN: Joi.string().required(),
     }),
   }),
-  addMovie
+  addMovie,
 );
 
 module.exports = router;
